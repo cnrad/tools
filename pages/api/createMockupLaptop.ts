c08721652461dda5d03d47be45cc89786f590e69
Conrad <conradwcrawford@gmail.com>
2021-07-02 13:48:56 -0400

    working with sharp

--- pages/api/createMockupLaptop.ts
+++ pages/api/createMockupLaptop.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { createCanvas, loadImage } from 'canvas';
+// import { createCanvas, loadImage } from 'canvas';
+import sharp from 'sharp';
+import axios from 'axios';
 
 type Data = {
   url: string
@@ -16,16 +18,46 @@ export default async function handler(
 
     console.log(url);
 
-    const canvas = createCanvas(2002, 1140);
-    const ctx = canvas.getContext('2d');
+    const input = (await axios({ url: url, responseType: "arraybuffer" })).data as Buffer;
+    const frame = (await axios({ url: "https://tools.cnrad.dev/mockup/MacbookLight.png", responseType: "arraybuffer" })).data as Buffer;
 
-    let frame = await loadImage('https://tools.cnrad.dev/mockup/MacbookLight.png');
-    ctx.drawImage(frame, 0, 0, 2002, 1140);
+    // const canvas = createCanvas(2002, 1140);
+    // const ctx = canvas.getContext('2d');
+
+    // let frame = await loadImage('https://tools.cnrad.dev/mockup/MacbookLight.png');
+    // ctx.drawImage(frame, 0, 0, 2002, 1140);
     
-    let screen = await loadImage(url);
-    ctx.drawImage(screen, 229, 65, 1542, 965);
+    // let screen = await loadImage(url);
+    // ctx.drawImage(screen, 229, 65, 1542, 965);
 
-    let data = await canvas.toDataURL();
+    // let data = await canvas.toDataURL();
 
-    res.status(200).json({url: data});
+    // res.status(200).json({url: data});
+
+    let outputB64 = "";
+
+    await sharp(input)
+        .resize({width: 1542, height: 965, fit: sharp.fit.fill})
+        .toBuffer() 
+        .then(async data => {
+
+            await sharp(frame)
+                .resize(2002, 1140)
+                .composite([{
+                    input: data,
+                    top: 65,
+                    left: 229
+                }])
+                .png()
+                .toBuffer()
+                .then(data => {
+                    outputB64 = data.toString('base64');
+                });
+
+        })
+        .catch(err => { 
+            console.log("Error: ", err);
+        });
+
+    res.json({url: `data:image/png;base64,${outputB64}`})
 }
